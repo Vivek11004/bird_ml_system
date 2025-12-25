4089f470dfcb858641d868bc560218c9241f6c67
Vivek11004 <149058272+Vivek11004@users.noreply.github.com>
2025-12-25 15:59:40 +0530

    Update detector.py

--- app/detector.py
+++ app/detector.py
@@ -1,18 +1,33 @@
 from ultralytics import YOLO
+import cv2
 
 class YOLOBirdDetector:
     def __init__(
         self,
-        model_path="runs/detect/train/weights/best.pt",
-        conf_thresh=0.4
+        model_path="models/best.pt",
+        conf_thresh=0.4, #
+        iou=0.6,
+        imgsz=896
     ):
         self.model = YOLO(model_path)
         self.conf_thresh = conf_thresh
+        self.iou = iou
+        self.imgsz = imgsz
 
     def detect(self, frame):
-        results = self.model(frame, conf=self.conf_thresh)[0]
-        detections = []
+        # 🔹 Light contrast enhancement (helps low-contrast & far birds)
+        frame = cv2.convertScaleAbs(frame, alpha=1.2, beta=15)
 
+        results = self.model(
+            frame,
+            conf=self.conf_thresh,
+            iou=self.iou,
+            imgsz=self.imgsz,
+            augment=True,     #  Test Time Augmentation
+            verbose=False
+        )[0]
+
+        detections = []
         for box in results.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
             conf = float(box.conf[0])
